f5cfe00ff4bd70a58d56ee894a039a7a078b188c
Abdelrahman Ragab <abdelrahmanragab@192.168.1.7>
2021-10-15 21:03:36 +0200

    add job views and urls

--- job/models.py
+++ job/models.py
@@ -13,12 +13,20 @@ class Job(models.Model):
     job_type = models.CharField(max_length=15,choices= JOB_TYPE)
     description = models.TextField(max_length=265)
     published_at = models.DateTimeField(auto_now=True)
-    Vacancy = models.IntegerField(default=1)
+    vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
     
 
